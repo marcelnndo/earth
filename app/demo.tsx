73aaa271c25123c1cfc0672d5140281ef80f4419
marcelnndo <celzlino29@gmail.com>
2026-08-13 09:55:00 +0700

    Menambahkan komponen globe dan setup project

---
+++ app/demo.tsx
@@ -0,0 +1,9 @@
+import RotatingEarth from "@/components/ui/wireframe-dotted-globe";
+
+export default function DemoOne() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-neutral-950 p-4">
+      <RotatingEarth width={800} height={600} />
+    </div>
+  );
+}
