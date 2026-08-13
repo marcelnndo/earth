bb6f2b1ffbda72150faea0f8a7a10f4e5afb574e
marcelnndo <celzlino29@gmail.com>
2026-08-13 10:03:43 +0700

    Menambahkan file konfigurasi Next.js untuk GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Baris ini penting untuk GitHub Pages
+  images: { unoptimized: true }
+}
+module.exports = nextConfig
